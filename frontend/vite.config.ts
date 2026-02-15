d2c4d753cb188a46813ef736c760f341b624b1cd
Rhey Louie Miaga <mrheylouie@gmail.com>
2026-02-15 15:55:29 +0800

    Fixing Refresh error

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,8 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: './',
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
+      }
+    }
+  }
 })
