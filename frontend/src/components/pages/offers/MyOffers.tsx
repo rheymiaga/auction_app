64818fddb9d66e15709562e1cc3302f9b7b706b1
Rhey Louie Miaga <mrheylouie@gmail.com>
2026-02-16 20:22:30 +0800

    Myoffers Placeholders

--- frontend/src/components/pages/offers/MyOffers.tsx
+++ frontend/src/components/pages/offers/MyOffers.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import api from "../../../services/api";
-import imagePlaceholder from "../../../assets/imagePlaceholder.jpg";
+
 
 interface MyOffer {
     id: number;
