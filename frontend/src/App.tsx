5aed950b45a26e7f5e77c5071e9c966d752247d9
Rhey Louie Miaga <mrheylouie@gmail.com>
2026-02-16 23:00:25 +0800

    updated loader

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,6 +9,8 @@ import { ItemForm } from "./components/pages/items/ItemForm";
 import Marketplace from "./components/pages/marketplace/MarketPlace";
 import { MyOffers } from "./components/pages/offers/MyOffers";
 import api from "./services/api";
+import { SiExpertsexchange } from "react-icons/si";
+import { RiLoader2Fill } from "react-icons/ri";
 
 interface User {
   id: string;
@@ -47,22 +49,33 @@ function App() {
 
   if (loading) {
     return (
-      <div className="min-h-screen flex flex-col items-center justify-center bg-linear-to-tr from-gray-900 via-purple-900 to-black text-white">
-        <div className="relative w-24 h-24">
-          <div className="absolute inset-0 rounded-full border-4 border-purple-400 border-t-transparent animate-spin"></div>
-          <div className="absolute inset-2 rounded-full border-4 border-pink-400 border-b-transparent animate-spin-slow"></div>
+      <div className="min-h-screen flex flex-col items-center justify-center bg-linear-to-br from-gray-900 via-black to-gray-800 relative overflow-hidden">
+        <div className="relative w-28 h-28 flex items-center justify-center animate-[float_6s_ease-in-out_infinite]">
+
+          <div className="absolute inset-0 rounded-full bg-linear-to-br from-yellow-400 via-yellow-500 to-orange-500 
+                        shadow-[inset_-6px_-6px_12px_rgba(255,255,255,0.2),inset_6px_6px_12px_rgba(0,0,0,0.4)]">
+          </div>
+
+          <span className="text-5xl font-bold text-white drop-shadow-[0_0_10px_rgba(255,255,255,0.6)]"><SiExpertsexchange /></span>
+
+          <div className="absolute inset-0 rounded-full bg-linear-to-t from-transparent via-white/20 to-transparent 
+                        animate-[scan_2.5s_linear_infinite]"></div>
+
+          <div className="absolute -inset-2 rounded-full border-2 border-yellow-400/40 blur-md animate-pulse"></div>
         </div>
 
-        <p className="mt-8 text-2xl font-bold tracking-wide text-transparent bg-clip-text bg-linear-to-r from-purple-300 via-pink-400 to-yellow-300 animate-pulse">
-          Preparing Auction_Xpress...
-        </p>
+        <div className="absolute top-1/3 right-1/3 text-pink-400 text-3xl animate-spin">
+          <RiLoader2Fill />
+        </div>
 
-        <p className="mt-2 text-sm text-gray-300 animate-fadeIn">
-          Please wait while we set things up
+        <p className="mt-10 text-xl md:text-2xl font-semibold tracking-wide text-transparent bg-clip-text 
+                   bg-linear-to-r from-yellow-400 via-orange-400 to-pink-400 animate-pulse">
+          Preparing Auction_Xpress...
         </p>
       </div>
     );
   }
+
 
 
   const hideNavbar = ["/login", "/register"].includes(location.pathname);
